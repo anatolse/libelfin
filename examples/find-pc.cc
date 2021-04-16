78fd8693f70bf8e8bb06320579a9e1bef8481102
anatol <anatol@beam-mw.com>
2021-04-16 18:56:36 +0300

    added CMake files and built on MSVC

--- examples/find-pc.cc
+++ examples/find-pc.cc
@@ -1,4 +1,4 @@
-#include "elf++.hh"
+
 #include "dwarf++.hh"
 
 #include <errno.h>
@@ -6,7 +6,139 @@
 #include <string>
 #include <inttypes.h>
 
+#include <fstream>
+#include <cstdint>
+#include <vector>
+#include <array>
+#include <map>
+
 using namespace std;
+
+class wasm_loader : public dwarf::loader
+{
+public:
+    using ByteBuffer = std::vector<uint8_t>;
+    using ByteBufferIt = ByteBuffer::const_iterator;
+    struct section
+    {
+        std::string name;
+        ByteBufferIt start;
+        ByteBufferIt end;
+        const uint8_t* data() const
+        {
+            return &(*start);
+        }
+        size_t size() const
+        {
+            return std::distance(start, end);
+        }
+    };
+
+    template <typename T, bool bSigned>
+    T ReadInternal(ByteBufferIt& it)
+    {
+        static_assert(!std::numeric_limits<T>::is_signed); // the sign flag must be specified separately
+
+        T ret = 0;
+        for (unsigned int nShift = 0; ; )
+        {
+            uint8_t n = Read1(it);
+            bool bEnd = !(0x80 & n);
+            if (!bEnd)
+                n &= ~0x80;
+
+            ret |= T(n) << nShift;
+            nShift += 7;
+
+            if (bEnd)
+            {
+                if constexpr (bSigned)
+                {
+                    if (0x40 & n)
+                        ret |= (~static_cast<T>(0) << nShift);
+                }
+                break;
+            }
+
+            //Test(nShift < sizeof(ret) * 8);
+        }
+
+        return ret;
+    }
+
+    uint8_t Read1(ByteBufferIt& it)
+    {
+        return Consume<uint8_t>(it);
+    }
+
+    template<typename T>
+    T Consume(ByteBufferIt& it)
+    {
+        T res = *reinterpret_cast<const T*>(&(*it));
+        std::advance(it, sizeof(T));
+        return res;
+    };
+    wasm_loader(const std::string& path)
+    {
+        {
+            std::fstream f(path, std::ios_base::binary | std::ios_base::in);
+            f.seekg(0, std::ios_base::end);
+            size_t size = f.tellg();
+            f.seekg(0);
+            m_buffer.resize(size);
+            f.read(reinterpret_cast<char*>(&m_buffer[0]), size);
+        }
+        constexpr std::array<uint8_t, 4> sig = { 0, 'a', 's', 'm' };
+        constexpr std::array<uint8_t, 4> version = { 1, 0, 0, 0 };
+        auto it = m_buffer.begin();
+        if (!std::equal(sig.begin(), sig.end(), it))
+            throw std::runtime_error("");
+        std::advance(it, sig.size());
+        if (!std::equal(version.begin(), version.end(), it))
+            throw std::runtime_error("");
+        std::advance(it, version.size());
+
+        while (it != m_buffer.end())
+        {
+            uint8_t type = Consume<uint8_t>(it);
+            uint32_t size = ReadInternal<uint32_t, false>(it);
+            auto end = it + size;
+
+            if (type == 0)
+            {
+                auto& s = m_sections.emplace_back();
+                uint32_t nameSize = ReadInternal<uint32_t, false>(it);
+                s.start = it + nameSize;
+                s.end = end;
+                s.name = { it, it + nameSize };
+            }
+
+            it = end;
+        }
+
+
+    }
+
+    const void* load(dwarf::section_type section, size_t* size_out)
+    {
+        const auto& name = dwarf::elf::section_type_to_name(section);
+        auto it = std::find_if(m_sections.begin(), m_sections.end(),
+            [&](const auto& s)
+        {
+            return s.name == name;
+        });
+
+        if (it == m_sections.end())
+            return nullptr;
+
+        *size_out = it->size();
+        return it->data();
+    }
+private:
+    ByteBuffer m_buffer;
+    std::vector<section> m_sections;
+};
+
 
 void
 usage(const char *cmd) 
@@ -71,15 +203,15 @@ main(int argc, char **argv)
                 usage(argv[0]);
         }
 
-        int fd = open(argv[1], O_RDONLY);
-        if (fd < 0) {
-                fprintf(stderr, "%s: %s\n", argv[1], strerror(errno));
-                return 1;
-        }
-
-        elf::elf ef(elf::create_mmap_loader(fd));
-        dwarf::dwarf dw(dwarf::elf::create_loader(ef));
-
+        //int fd = open(argv[1], O_RDONLY);
+        //if (fd < 0) {
+        //        fprintf(stderr, "%s: %s\n", argv[1], strerror(errno));
+        //        return 1;
+        //}
+        //
+        //elf::elf ef(elf::create_mmap_loader(fd));
+        //dwarf::dwarf dw(dwarf::elf::create_loader(ef));
+        dwarf::dwarf dw(std::make_shared<wasm_loader>(argv[1]));
         // Find the CU containing pc
         // XXX Use .debug_aranges
         for (auto &cu : dw.compilation_units()) {
